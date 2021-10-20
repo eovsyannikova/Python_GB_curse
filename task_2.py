c5505a67fb84e7ab2a4257b0b1990352b8aaef16
eovsyannikova <92162060+eovsyannikova@users.noreply.github.com>
2021-10-20 19:08:05 +0300

    Delete task_2.py

--- task_2.py
+++
@@ -1,8 +0,0 @@
-while True:
-    number=int(input('Введите число: '))
-    if 0<number<10 :
-        number=number**2
-        print('Результат: ', number)
-        break
-    else:
-        print('Диапазон допустимых значений от 0 до 10 (не включительно)')
