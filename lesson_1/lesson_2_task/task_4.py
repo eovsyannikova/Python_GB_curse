5b07699960475f471baa560ed3f315a997c6318f
Ekaterina <k.kardashova@gmail.com>
2021-10-25 19:48:13 +0300

    - task 3 task 4

---
+++ lesson_1/lesson_2_task/task_4.py
@@ -0,0 +1,10 @@
+
+input_str = input('Введите слова через пробел: ')
+# Разбираем введенные значения в список
+list_str = input_str.split()
+print(list_str)
+# используем функцию enumerate для нумерации
+for ind, el in enumerate(list_str, 1):
+    # используем тернарный оператор для проверки кол-ва символов
+    el = (el, el[0:10])[len(el) > 10]
+    print(ind, el)
