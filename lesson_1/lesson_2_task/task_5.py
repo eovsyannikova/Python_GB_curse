950b0a1348f8f65a0df1801dafc3b876ec89fdd2
Ekaterina <k.kardashova@gmail.com>
2021-10-26 20:38:23 +0300

    - task 5 task 6

---
+++ lesson_1/lesson_2_task/task_5.py
@@ -0,0 +1,14 @@
+my_list = [7, 5, 3, 3, 2]
+user_val = int(input('Ведите место в рейтинге: '))
+# Проверяем есть ли элемент в списке
+if user_val in my_list:
+    i = my_list.index(user_val)
+    count = my_list.count(user_val)
+    my_list.insert(i + count, user_val)
+else:
+    my_list.append(user_val)
+    my_list.sort(reverse=True)
+
+print(my_list)
+
+
