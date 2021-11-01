67c644d85e384de65d279b098324b22ae5feaaa0
Ekaterina <k.kardashova@gmail.com>
2021-11-01 21:40:14 +0300

    - task 1 (1 var)

--- lesson_4/task_1.py
+++ lesson_4/task_1.py
@@ -1,9 +1,4 @@
 from sys import argv
 
 name_script, hour, rate, bonus = argv
-# param_list = [float(argv[i + 1]) for i in range(len(argv) - 1)]
-# hour = param_list[0]
-# rate = param_list[1]
-# bonus = param_list[2]
-# rez = hour * rate + bonus
 print(f"Премия сотруднику составляет: {float(hour) * float(rate) + float(bonus)} ")
